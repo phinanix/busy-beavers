3cba001e809c1026e79b24a6193b7b5f450cc76d
Seraphina Nix <seraphina.nix@gmail.com>
2020-08-14 23:22:11 -0700

    wrote the pile of functions needed to enumerate all turing machines

--- src/Beaver.hs
+++ src/Beaver.hs
@@ -5,14 +5,15 @@ import qualified Relude.Unsafe as Unsafe hiding (at)
 
 --the Phase a turing machine is in, to not conflict with State
 data Phase = Halt | Phase Int deriving (Eq, Ord, Show)
-data Dir = L | R deriving (Eq, Show)
+data Dir = L | R deriving (Eq, Ord, Show)
 type Bit = Bool
+type Trans = (Phase, Bit, Dir)
 
 --a Turing machine
 data Turing = Turing
   { states :: Int --the number of states a machine has
-  , transitions :: Map (Phase, Bit) (Phase, Bit, Dir)
-  }
+  , transitions :: Map (Phase, Bit) Trans
+  } deriving (Eq, Ord, Show)
 
 data Zipper a = Zipper
   { left :: [a]
@@ -50,3 +51,75 @@ simStep (Turing _ trans ) (phase@(Phase _), (Zipper l bit r))
         Right $ (newPhase, Unsafe.fromJust $ zipLeft $ Zipper l newBit r)
       Just (newPhase, newBit, R) ->
         Right $ (newPhase, Unsafe.fromJust $ zipRight $ Zipper l newBit r)
+
+--
+prevPhase :: Int -> Phase -> Phase
+prevPhase n Halt = Phase (n-1)
+prevPhase _ (Phase 0) = Halt
+prevPhase _ (Phase k) = Phase (k-1)
+
+nextPhase :: Int -> Phase -> Phase
+nextPhase _ Halt = Phase 0
+nextPhase n (Phase k) = if k == n-1 then Halt else Phase $ k+1
+
+nextEdge :: Int -> (Phase, Bit) -> (Phase, Bit)
+nextEdge _ (phase, False) = (phase, True)
+nextEdge n (phase, True) = (nextPhase n phase, False)
+
+prevEdge :: Int -> (Phase, Bit) -> (Phase, Bit)
+prevEdge n (phase, False) = (prevPhase n phase, True)
+prevEdge _ (phase, True) = (phase, False)
+
+firstTrans :: Int -> Trans
+firstTrans _ = (Halt, False, L)
+
+lastTrans :: Int -> Trans
+lastTrans n = (Phase (n-1), True, R)
+
+nextTrans :: Int -> Trans -> Trans
+nextTrans _ (phase, False, L) = (phase, False, R)
+nextTrans _ (phase, False, R) = (phase, True, L)
+nextTrans _ (phase, True, L) = (phase, True, R)
+nextTrans n (phase, True, R) = (nextPhase n phase, False, L)
+
+prevTrans :: Int -> Trans -> Trans
+prevTrans n (phase, False, L) = (prevPhase n phase, True, R)
+prevTrans _ (phase, False, R) = (phase, False, L)
+prevTrans _ (phase, True, L) = (phase, False, R)
+prevTrans _ (phase, True, R) = (phase, True, L)
+
+transList :: Int -> [(Phase, Bit)]
+transList n = ((,False) <$> allPhases n) ++ ((,True) <$> allPhases n)
+
+allPhases :: Int -> [Phase]
+allPhases n = Halt : (Phase <$> [0.. n-1])
+
+--the lexicographically first turing machine of a certain size
+firstTM :: Int -> Turing
+firstTM n = if n < 1 then error "TM must have at least one state" else
+  Turing n $ fromList $ (\k -> (k, firstTrans n)) <$> transList n
+
+lastTM :: Int -> Turing
+lastTM n = if n < 1 then error "TM must have at least one state" else
+  Turing n $ fromList $ (\k -> (k, lastTrans n)) <$> transList n
+
+--returns "Nothing" exactly when called on lastTM
+nextTM :: Turing -> Maybe Turing
+nextTM t@(Turing n tMap) = if t == (lastTM n) then Nothing else
+  Just $ Turing n $ nextTMap tMap where
+  nextTMap :: Map (Phase, Bit) Trans -> Map (Phase, Bit) Trans
+  nextTMap = nextAtTrans (Phase (n-1), True)
+  nextAtTrans :: (Phase, Bit) -> Map (Phase, Bit) Trans -> Map (Phase, Bit) Trans
+  nextAtTrans edge m = case m ^. at edge of
+    Just trans -> if trans == lastTrans n then
+      --wrap around to first trans, and increment the next counter up
+      nextAtTrans (prevEdge n edge) $ m & at edge ?~ firstTrans n else
+      --just increment the current transition
+      m & at edge ?~ nextTrans n trans
+    Nothing -> error "nextTM is only defined on total turing machines"
+universeTM :: Int  -> [Turing]
+universeTM n = recurse $ Just $ firstTM n where
+  --the list starting with t, going until the lastTM, or the empty list
+  recurse :: Maybe Turing -> [Turing]
+  recurse Nothing = []
+  recurse (Just t) = t : recurse (nextTM t)
