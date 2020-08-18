dff93c0291da0fed3de4fa8fe51cea6c39a43ce8
Seraphina Nix <seraphina.nix@gmail.com>
2020-08-18 10:58:28 -0700

    refactored Tape to be infinite

--- src/Beaver.hs
+++ src/Beaver.hs
@@ -1,14 +1,12 @@
 module Beaver where
 import Control.Lens
-import Relude
-import qualified Relude.Unsafe as Unsafe hiding (at)
+import Relude hiding (state)
 
 --the Phase a turing machine is in, to not conflict with State
-data Phase = Halt | Phase Int deriving (Eq, Ord, Show)
 data Dir = L | R deriving (Eq, Ord, Show)
 type Bit = Bool
 type Edge = (Int, Bit)
-type Trans = (Phase, Bit, Dir)
+data Trans = Halt | Step Int Bit Dir deriving (Eq, Ord, Show)
 --TODO :: Define a type edge, that is just an Int and a Bool, the two things read from the tape,
 -- and modify the enumeration function so that it does not put Halt in the transition map
 
@@ -20,47 +18,6 @@ data Turing = Turing
   , transitions :: Map Edge Trans
   } deriving (Eq, Ord, Show)
 
-data Zipper a = Zipper
-  { left :: [a]
-  , point :: a
-  , right :: [a]
-  }
-
---functions to move the point of the zipper left and right
---returning nothing if the list ends
-zipLeft :: Zipper a -> Maybe (Zipper a)
-zipLeft (Zipper [] _ _) = Nothing
-zipLeft (Zipper (x : xs) point right) = Just $ Zipper xs x (point : right)
-
-zipRight :: Zipper a -> Maybe (Zipper a)
-zipRight (Zipper _ _ []) = Nothing
-zipRight (Zipper left point (x : xs)) = Just $ Zipper (point : left) x xs
-
-type Tape = Zipper Bit
-type SimState = (Int, Tape)
-
-initState :: SimState
-initState = (0, Zipper (repeat False) False (repeat False))
-
---returns right if simulation was successful
---returns left and the unknown edge if we don't know how to make progress
---from the current state, because it isn't in the transition map, or
---returns only the final tape if the machine halts, or returns the new state
-simStep :: Turing -> SimState -> Either Edge (Either Tape SimState)
-simStep (Turing _ trans ) (i, (Zipper l bit r))
-  = case trans ^. at (i, bit) of
-      Nothing -> Left (i, bit)
-      --these fromJusts are safe because the tape is infinite
-      Just (Halt, newBit, L) ->
-        Right $ Left $ Unsafe.fromJust $ zipLeft $ Zipper l newBit r
-      Just (Halt, newBit, R) ->
-        Right $ Left $ Unsafe.fromJust $ zipRight $ Zipper l newBit r
-      Just (Phase j, newBit, L) ->
-        Right $ Right $ (j, Unsafe.fromJust $ zipLeft $ Zipper l newBit r)
-      Just (Phase j, newBit, R) ->
-        Right $ Right $ (j, Unsafe.fromJust $ zipRight $ Zipper l newBit r)
-
---
 
 uniDir :: NonEmpty Dir
 uniDir = L :| [R]
@@ -72,9 +29,6 @@ uniBit = False :| [True]
 uniFin :: Int -> NonEmpty Int
 uniFin n = 0 :| [1.. n-1]
 
-uniPhase :: Int -> NonEmpty Phase
-uniPhase n = Halt :| toList (Phase <$> uniFin n)
-
 uniEdge :: Int -> NonEmpty Edge
 uniEdge n = do
   i <- uniFin n
@@ -82,11 +36,11 @@ uniEdge n = do
   pure (i, bit)
 
 uniTrans :: Int -> NonEmpty Trans
-uniTrans n = do
-  phase <- uniPhase n
+uniTrans n = Halt :| toList (do
+  i <- uniFin n
   bit <- uniBit
   dir <- uniDir
-  pure (phase, bit, dir)
+  pure $ Step i bit dir)
 
 --this is where it starts to become obvious universe is a typeclass
 --but many of mine are parameterized on an Int and that sounds annoying to deal with
@@ -111,3 +65,60 @@ uni3size = length $ uniTuring 3
 
 uni4size :: Int
 uni4size = length $ uniTuring 4
+
+data Tape = Tape
+  { left :: [Bit]
+  , head :: Bit
+  , right :: [Bit]
+  }
+
+--functions to move the point of the zipper left and right
+--returning nothing if the list ends
+tapeLeft :: Tape -> Tape
+tapeLeft (Tape [] h rs) = Tape [] False (h : rs)
+tapeLeft (Tape (l : ls) h rs) = Tape ls l (h : rs)
+
+tapeRight :: Tape -> Tape
+tapeRight (Tape ls h []) = Tape (h : ls) False []
+tapeRight (Tape ls h (r : rs)) = Tape (h : ls) r rs
+
+dispTape :: Tape -> String
+dispTape (Tape ls h rs) = show (reverse ls) <> "  [" <> show h <> "]  " <> show rs
+
+type SimState = (Int, Tape)
+dispSimState :: SimState -> String
+dispSimState (i, tape) = "state: " <> show i <> " tape: " <> dispTape tape
+
+--Unknown means we don't know how to make progress
+--from the current state, because it isn't in the transition map
+--Stop means the machine halted with the given tape
+--Continue means the machien hasn't halted and the current state is given
+data SimResult = Unknown Edge | Stop Tape | Continue SimState
+
+initState :: SimState
+initState = (0, Tape [] False [])
+
+simStep :: Turing -> SimState -> SimResult
+simStep (Turing _ trans ) (i, (Tape ls bit rs))
+  = case trans ^. at (i, bit) of
+    Nothing -> Unknown (i, bit)
+    --we assume WLOG that the machine goes left and writes True when it halts
+    Just Halt ->
+      Stop $ tapeLeft $ Tape ls True rs
+    Just (Step j newBit L) ->
+      Continue $ (j, tapeLeft $ Tape ls newBit rs)
+    Just (Step j newBit R) ->
+      Continue $ (j, tapeRight $ Tape ls newBit rs)
+
+--simulates a machine for the given number of steps
+simulate :: Int -> SimState -> Turing -> SimResult
+simulate ( (\i -> i <= 0) -> True) state _ = Continue state
+simulate steps state t = case simStep t state of
+  result@(Unknown _) -> result
+  result@(Stop _) -> result
+  (Continue newState) -> simulate (steps - 1) newState t
+
+dispResult :: SimResult -> String
+dispResult (Unknown edge) = "Simulating got stuck on " <> show edge
+dispResult (Stop tape) = "Simulating finished with the tape: " <> dispTape tape
+dispResult (Continue state) = "Simulating didn't finish, we reached state: " <> dispSimState state
